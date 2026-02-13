4a548f9028485cdfbe17233e4818342b22c8ac32
Manas Ranjan Murmu <manasranjanmurmu@gmail.com>
2026-02-13 14:13:25 +0530

    Adding one more to TRigger the Jenkins

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello from Jenkins Pipeline 🚀");
+    res.send("This is a sample Node.js application to demonstrate Jenkins Pipeline integration.");
+
+    res.send("Feel free to explore and modify the code as needed!");    
 });
 
 app.listen(3000, () => {
